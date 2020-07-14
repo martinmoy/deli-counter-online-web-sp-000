9cddb18161980a526ec167a0cbe34545cde09b0c
Martin Moy <martinmoy1991@gmail.com>
2020-07-14 01:06:08 +0000

    Done.

--- deli_counter.rb
+++ deli_counter.rb
@@ -1,13 +1,16 @@
+
+
 def take_a_number(katz_deli, name)
   katz_deli.push(name)
   puts "Welcome, #{name}. You are number #{katz_deli.length} in line."
 end
 
-def line(katz_deli)
-  if katz_deli.length == 0
+def line(kx)
+
+  if kx.length == 0
     puts "The line is currently empty."
   else
-    katz_deli.each.with_index do |name, index|
+    kx.each.with_index do |name, index|
       katz_deli.push("#{index+1}. #{name}")
     end
     puts "The line is currently: #{katz_deli.join(" ")}"
